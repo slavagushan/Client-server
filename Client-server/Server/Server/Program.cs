89d611fdbe69dc4c38aafab2f46bfaeb249f9932
slavagushan <90864156+slavagushan@users.noreply.github.com>
2021-12-27 12:34:12 +0200

    Client-server
    Client-server

---
+++ Client-server/Server/Server/Program.cs
@@ -0,0 +1,74 @@
+﻿using RandomDataGenerator.FieldOptions;
+using RandomDataGenerator.Randomizers;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+	class Program
+	{
+        static int port = 8005; // порт для приема входящих запросов
+        static void Main(string[] args)
+        {
+            // получаем адреса для запуска сокета
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+            // создаем сокет
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                // связываем сокет с локальной точкой, по которой будем принимать данные
+                listenSocket.Bind(ipPoint);
+
+                // начинаем прослушивание
+                listenSocket.Listen(10);
+
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байтов
+                    byte[] data = new byte[256]; // буфер для получаемых данных
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+                    // отправляем ответ
+                    //string message = "ваше сообщение доставлено";
+                    //data = Encoding.Unicode.GetBytes(message);
+                    //handler.Send(data);
+
+                    int max = int.Parse(builder.ToString());
+
+                    for (int i = 0; i < max; i++)
+					{
+                        System.Threading.Thread.Sleep(50);
+                        var randomizerWords = RandomizerFactory.GetRandomizer(new FieldOptionsTextWords() { Min = max, Max = max });
+                        string words = randomizerWords.Generate();
+                        data = Encoding.Unicode.GetBytes(words);
+                        handler.Send(data);
+                    }
+
+                    // закрываем сокет
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
